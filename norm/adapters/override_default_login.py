d98117218a4463572a485673a5f2f27fb92c050b
Ryan Chausse <chausse@gmail.com>
2022-02-23 22:07:25 -0800

    Changed: renamed project and app; some UI changes

---
+++ norm/adapters/override_default_login.py
@@ -0,0 +1,14 @@
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from allauth.account.utils import user_email
+from django.http import HttpResponse
+from django.forms import ValidationError
+
+
+class NormSocialAccountAdapter(DefaultSocialAccountAdapter):
+    def pre_social_login(self, request, sociallogin):
+        email_domain = user_email(sociallogin.user).split("@")[1]
+        if email_domain != 'gmail.com' or email_domain != 'hendricksenphd.com':
+            return HttpResponse('Please log in with a Google account that ends \
+                                with "@gmail.com" or "@hendricksenphd.com". You \
+                                may have to log out of your current Gmail account. \
+                                Try again at <a href="./">Norm Form</a>')
