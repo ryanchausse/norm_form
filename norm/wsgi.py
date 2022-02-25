7c890f8b6ef1695a639eab3d7afa3a3d1729e5f0
Ryan Chausse <chausse@gmail.com>
2022-02-24 16:13:25 -0800

    Changed: added venv site-packages to wsgi.py

--- norm/wsgi.py
+++ norm/wsgi.py
@@ -7,9 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.insert(0, "/var/www/venv_norm/lib/python3.8/site-packages")
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'norm.settings')
 
