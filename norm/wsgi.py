6f16b9dbe9c022866c71343998c6cc06db36e899
Ryan Chausse <chausse@gmail.com>
2022-02-24 16:38:13 -0800

    Removed: wrong configuration line from wsgi.py

--- norm/wsgi.py
+++ norm/wsgi.py
@@ -7,11 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
-import os, sys
+import os
 
 from django.core.wsgi import get_wsgi_application
-
-sys.path.insert(0, "/var/www/venv_norm/lib/python3.8/site-packages")
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'norm.settings')
 
