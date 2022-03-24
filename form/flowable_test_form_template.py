0c901cecd76dae8db15480923d42d85d9612f7f6
Ryan Chausse <chausse@gmail.com>
2022-03-23 19:42:11 -0700

    Changed: More work done on Flowable objects, changing Interactive Checkboxes to NonInteractive via a new class

--- form/flowable_test_form_template.py
+++ form/flowable_test_form_template.py
@@ -1,10 +1,15 @@
 # Sample platypus document
 # From the FAQ at reportlab.org/oss/rl-toolkit/faq/#1.1
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle, Flowable
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle, Flowable, PageTemplate
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.units import inch
 from reportlab.lib.enums import TA_LEFT, TA_CENTER, TA_RIGHT
+from reportlab.platypus import BaseDocTemplate, PageBreak, FrameBreak, Frame
+from reportlab.pdfgen import canvas
+from reportlab.platypus import flowables
+from reportlab.graphics.shapes import Rect
+from reportlab.graphics.shapes import Drawing
 
 
 width, height = letter
@@ -13,36 +18,44 @@ width, height = letter
 pageinfo = "Dr. Hendricksen evaluation form"
 
 
-class InteractiveCheckBox(Flowable):
-    def __init__(self, name, tooltip="", checked=False, size=12, button_style="check"):
+class UncheckedBox(Flowable):
+    def __init__(self, width=10, height=10):
         Flowable.__init__(self)
-        self.name = name
-        self.tooltip = tooltip
-        self.size = size
-        self.checked = checked
-        self.buttonStyle = button_style
+        self.width = width
+        self.height = height
 
     def draw(self):
-        self.canv.saveState()
-        form = self.canv.acroForm
-        form.checkbox(checked=self.checked,
-                      buttonStyle=self.buttonStyle,
-                      name=self.name,
-                      tooltip=self.tooltip,
-                      relative=True,
-                      size=self.size)
-        self.canv.restoreState()
+        self.canv.rect(0, 2, self.width, self.height, fill=0)
 
 
-def first_page(canvas, doc):
-    canvas.saveState()
-    canvas.setFont('Helvetica', 9)
-    canvas.restoreState()
+class CheckedBox(Flowable):
+    def __init__(self, width=10, height=10):
+        Flowable.__init__(self)
+        self.width = width
+        self.height = height
+
+    def draw(self):
+        self.canv.rect(0, 2, self.width, self.height, fill=0)
+        self.canv.drawCentredString(0.5 * self.width, 0.5 * self.height, '✓')
+
+
+class NormFormDocTemplate(BaseDocTemplate):
+    def build(self, flowables):
+        self._calc()
+        print(self.height)  # 821.89
+        print(self.width)  # 565.28
+        frame_top = Frame(self.leftMargin, self.bottomMargin, self.width, self.height, id='frame_top')
+        frame_left = Frame(self.leftMargin, -158, 115, self.height, id='frame_left')
+        frame_right = Frame(self.leftMargin + 110, -155, 450, self.height, id='frame_right')
+        self.addPageTemplates([PageTemplate(id='First', frames=[frame_top, frame_left, frame_right], pagesize=self.pagesize)])
+        BaseDocTemplate.build(self, flowables)
 
 
 def build_form():
-    doc = SimpleDocTemplate("templates/forms/flowable_test.pdf", rightMargin=15, leftMargin=15,
-                            topMargin=10, bottomMargin=10)
+    doc = NormFormDocTemplate("templates/forms/flowable_test.pdf", rightMargin=15, leftMargin=15,
+                              topMargin=10, bottomMargin=10)
+    # doc = SimpleDocTemplate("templates/forms/flowable_test.pdf", rightMargin=15, leftMargin=15,
+    #                         topMargin=10, bottomMargin=10)
     story = []
     title_text = "Psychologist Consultation / Follow-Up"
     p = Paragraph(title_text, style=ParagraphStyle(name='Normal', fontName='Helvetica', fontSize=12,
@@ -81,45 +94,119 @@ def build_form():
 
     story.append(Spacer(0, 0.1 * inch))
 
-    # Checkbox fields
-    # p = Paragraph(f'Aggressive Behavior:',
-    #               style=ParagraphStyle(name='Normal', fontName='Helvetica',
-    #                                    fontSize=9, alignment=TA_LEFT))
-    # story.append(p)
+    story.append(FrameBreak)
 
-    data = [['Aggressive Behavior',
-             'Physical:', InteractiveCheckBox("physical", "Physical"),
-             'Verbal:', InteractiveCheckBox("verbal", "Verbal"),
-             'Gestures:', InteractiveCheckBox("Gestures", "Gestures"),
-             'Threatening Behaviors:', InteractiveCheckBox("threatening_behaviors", "Threatening Behaviors"),
-             ]]
+    p = Paragraph('Aggressive Behavior<br /><br />'
+                  'General Appearance<br /><br />'
+                  'Treatment & Compliance<br /><br />'
+                  'Inappropriate Behavior<br /><br />'
+                  'Attitude<br /><br />'
+                  'Speech<br /><br />'
+                  'Verbal Abilities<br /><br />'
+                  'Communication<br /><br />'
+                  'Perceptual Disturbances<br /><br />'
+                  'Level of Consciousness<br /><br />'
+                  'Thought Process<br /><br />'
+                  'Thought Content<br /><br />'
+                  'Mood<br /><br />'
+                  'Affect<br /><br />'
+                  'Harmfulness<br /><br />'
+                  'Attention & Concentration<br /><br />'
+                  'Orientation<br /><br />'
+                  'Insight & Judgment<br /><br />'
+                  'Sleep Disturbance<br /><br />'
+                  'Appetite change',
+                  style=ParagraphStyle(name='Normal', fontName='Helvetica',
+                                       fontSize=8, alignment=TA_RIGHT))
+    story.append(p)
+
+    story.append(FrameBreak)
+
     grid = [
-        ('FONTNAME', (0, 0), (1, 0), 'Helvetica-Bold'),
         ('FONTNAME', (1, 0), (-1, -1), 'Helvetica'),
-        ('FONTSIZE', (0, 0), (-1, -1), 9),
-        ('ALIGN', (1, 0), (1, 0), 'RIGHT'),
-        ('ALIGN', (2, 0), (2, 0), 'LEFT'),
-        ('ALIGN', (3, 0), (3, 0), 'RIGHT'),
-        ('ALIGN', (4, 0), (4, 0), 'LEFT'),
-        ('ALIGN', (5, 0), (5, 0), 'RIGHT'),
-        ('ALIGN', (6, 0), (6, 0), 'LEFT'),
-        ('ALIGN', (7, 0), (7, 0), 'RIGHT'),
-        ('ALIGN', (8, 0), (8, 0), 'LEFT'),
-        ('ALIGN', (9, 0), (9, 0), 'RIGHT'),
-        ('ALIGN', (10, 0), (10, 0), 'LEFT'),
-        ('ALIGN', (11, 0), (11, 0), 'RIGHT'),
-        ('ALIGN', (12, 0), (12, 0), 'LEFT'),
-        ('ALIGN', (13, 0), (13, 0), 'RIGHT'),
-        ('ALIGN', (14, 0), (14, 0), 'LEFT'),
-        ('ALIGN', (15, 0), (15, 0), 'RIGHT'),
-        ('ALIGN', (16, 0), (16, 0), 'LEFT'),
-        ('ALIGN', (17, 0), (17, 0), 'RIGHT'),
-        ('ALIGN', (18, 0), (18, 0), 'LEFT'),
+        ('FONTSIZE', (0, 0), (-1, -1), 8),
+        ('ALIGN', (0, 0), (0, 0), 'RIGHT'),
+        ('ALIGN', (1, 0), (1, 0), 'LEFT'),
+        ('RIGHTPADDING', (0, 0), (0, 0), 2),
+        ('LEFTPADDING', (1, 0), (1, 0), 2),
+        ('ALIGN', (2, 0), (2, 0), 'RIGHT'),
+        ('ALIGN', (3, 0), (3, 0), 'LEFT'),
+        ('RIGHTPADDING', (2, 0), (2, 0), 2),
+        ('LEFTPADDING', (3, 0), (3, 0), 2),
+        ('ALIGN', (4, 0), (4, 0), 'RIGHT'),
+        ('ALIGN', (5, 0), (5, 0), 'LEFT'),
+        ('RIGHTPADDING', (4, 0), (4, 0), 2),
+        ('LEFTPADDING', (5, 0), (5, 0), 2),
+        ('ALIGN', (6, 0), (6, 0), 'RIGHT'),
+        ('ALIGN', (7, 0), (7, 0), 'LEFT'),
+        ('RIGHTPADDING', (6, 0), (6, 0), 2),
+        ('LEFTPADDING', (7, 0), (7, 0), 2),
+        ('ALIGN', (8, 0), (8, 0), 'RIGHT'),
+        ('ALIGN', (9, 0), (9, 0), 'LEFT'),
+        ('RIGHTPADDING', (8, 0), (8, 0), 2),
+        ('LEFTPADDING', (9, 0), (9, 0), 2),
+        ('ALIGN', (10, 0), (10, 0), 'RIGHT'),
+        ('ALIGN', (11, 0), (11, 0), 'LEFT'),
+        ('RIGHTPADDING', (10, 0), (10, 0), 2),
+        ('LEFTPADDING', (11, 0), (11, 0), 2),
+        ('ALIGN', (12, 0), (12, 0), 'RIGHT'),
+        ('ALIGN', (13, 0), (13, 0), 'LEFT'),
+        ('RIGHTPADDING', (12, 0), (12, 0), 2),
+        ('LEFTPADDING', (13, 0), (13, 0), 2),
+        ('ALIGN', (14, 0), (14, 0), 'RIGHT'),
+        ('ALIGN', (15, 0), (15, 0), 'LEFT'),
+        ('RIGHTPADDING', (14, 0), (14, 0), 2),
+        ('LEFTPADDING', (15, 0), (15, 0), 2),
+        ('ALIGN', (16, 0), (16, 0), 'RIGHT'),
+        ('ALIGN', (17, 0), (17, 0), 'LEFT'),
+        ('RIGHTPADDING', (16, 0), (16, 0), 2),
+        ('LEFTPADDING', (17, 0), (17, 0), 2),
+        ('ALIGN', (18, 0), (18, 0), 'RIGHT'),
+        ('ALIGN', (19, 0), (19, 0), 'LEFT'),
+        ('RIGHTPADDING', (18, 0), (18, 0), 2),
+        ('LEFTPADDING', (19, 0), (19, 0), 2),
+        ('BOTTOMPADDING', (0, -1), (-1, -1), 8.9)
     ]
-    t = Table(data, style=TableStyle(grid))
+
+    data = [[# 'Aggressive Behavior:',
+        # Eventually, do CheckedBox() if {{ physical }} else UncheckedBox
+            'Physical', CheckedBox(),
+            'Verbal', UncheckedBox(),
+            'Gestures', UncheckedBox(),
+            'Threatening Behaviors', UncheckedBox()
+    ]]
+    t = Table(data, style=TableStyle(grid), hAlign=TA_LEFT, colWidths=None)
     story.append(t)
 
-    doc.build(story, onFirstPage=first_page)
+    # Separate tables needed for formatting / length overruns on vertically justified columns
+    data = [[# 'General Appearance:',
+             'Well Groomed', CheckedBox(),
+             'Fairly Groomed', UncheckedBox(),
+             'Poorly Groomed', UncheckedBox(),
+             'Disheveled', UncheckedBox()
+    ]]
+    t = Table(data, style=TableStyle(grid), hAlign=TA_LEFT, colWidths=None)
+    story.append(t)
+
+    data = [[# 'Treatment & Compliance:',
+             'Acceptable', CheckedBox(),
+             'Low Motivation', UncheckedBox(),
+             'Resistive', UncheckedBox(),
+             'Argumentative', UncheckedBox(),
+             'Exit Seeking', UncheckedBox(),
+             'Wandering', UncheckedBox()
+    ]]
+    t = Table(data, style=TableStyle(grid), hAlign=TA_LEFT, colWidths=None)
+    story.append(t)
+
+    data = [[# 'Inappropriate Behavior:',
+             'Inappropriate Behavior', UncheckedBox(),
+             'Describe:', ' ', ' ', ' ', ' ']]
+    t = Table(data, style=TableStyle(grid), hAlign=TA_LEFT, colWidths=None)
+    story.append(t)
+
+    # doc.build(story, onFirstPage=first_page)
+    doc.build(story)
 
 
 if __name__ == "__main__":
