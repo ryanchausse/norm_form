7abf7815cdd89af621cd213b8b0ee0e02ee028ed
Ryan Chausse <chausse@gmail.com>
2022-04-29 01:12:33 -0700

    Changed: color of signature field to dark blue

--- form/forms.py
+++ form/forms.py
@@ -182,7 +182,7 @@ class NormFormForm(forms.ModelForm):
     current_medication = forms.CharField(required=False, max_length=50000, widget=forms.Textarea(attrs={'cols': 30, 'rows': 7}))
     discussion_treatment = forms.CharField(required=False, max_length=50000, widget=forms.Textarea(attrs={'cols': 30, 'rows': 7}))
 
-    signature = JSignatureField(widget=JSignatureWidget(jsignature_attrs={'color': '#000'}))
+    signature = JSignatureField(widget=JSignatureWidget(jsignature_attrs={'color': '#0c00a0'}))
 
     class Meta:
         model = NormForm
