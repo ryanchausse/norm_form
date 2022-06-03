93485eac35124e5ba141fcfc1eac3afda3fddf5c
Ryan Chausse <chausse@gmail.com>
2022-06-03 13:47:53 -0700

    Added: Bal's professional email to pre_social_login function

--- norm/adapters/override_default_login.py
+++ norm/adapters/override_default_login.py
@@ -13,6 +13,6 @@ class NormSocialAccountAdapter(DefaultSocialAccountAdapter):
                                 may have to log out of your current Gmail account. \
                                 Try again at <a href="./">Norm Form</a>')
         elif email_address not in ['chausse@gmail.com', 'paul@hendricksenlabs.com',
-                                   'norm@hendricksenphd.com']:
+                                   'norm@hendricksenphd.com', 'balbains@mbsbills.com']:
             return HttpResponse('You are not authorized to log in. \
                                 Try again at <a href="./">Norm Form</a>')
